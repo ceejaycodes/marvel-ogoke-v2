6483e11fa13d70c04472113dc91d696d17f837bf
Marvel <dreelceejay79@yahoo.com>
2023-11-26 09:40:53 +0100

    added more data

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -20,11 +20,22 @@ const About = (props: Props) => {
         <div className='flex my-18 py-10 flex-wrap text-slate-400 px-9 justify-evenly items-center'>
             <div className='lg:w-1/2 m-4'>
                 <p>
-                    « Technology is humanity’s most potent tool in her quest for survival and all round evolution. I am a software developer who believes he can use his skills to make an enormous impact.
-                    «« Although I appreciate the importance of Javascript and frontend technology, I am heavily interested in exploring the potential of Python and its backend frameworks to build scalable applications and software.
-                    «« I find nature fascinating and also enjoy a wide range of alternative music. In my spare time, I like to scribble my thoughts into words - it also makes for good poetry.
-                    «« I like to think that I possess problem solving skills that will benefit any organisation regardless of its scale. I appreciate good work ethic and positive spirited people.
-                    «« Looking forward to working on your projects or simply connecting over a cup of coffee.</p>
+                Welcome to my digital abode, where lines of code converge to craft innovative solutions and bring ideas to life.<br></br> 
+                I am Marvel Ogoke, a dedicated software engineer with a passion for creating elegant and efficient software systems.
+                With a foundation built on a robust blend of theoretical knowledge and practical experience, I thrive on transforming complex problems into streamlined, user-centric solutions.
+
+                <br></br> <strong>Professional Journey:</strong><br></br>
+My journey in the realm of software engineering has been marked by a commitment to continuous learning and a relentless pursuit of excellence. Leveraging a diverse skill set that spans front-end and back-end development, I have had the privilege of contributing to a variety of projects, from scalable web applications to intricate software architectures.
+
+<br></br><strong>Hobbies:</strong><br></br>
+Beyond the realm of coding, my life is enriched by a symphony of interests. As a pianist, I find solace and inspiration in the nuanced language of music, transcending the boundaries of technology to connect with the soul.
+ <br></br>Poetry serves as another expressive outlet, allowing me to distill complex emotions and experiences into the elegance of language.
+ <br></br>In the digital tapestry of my life, curating playlists is an art form that brings joy and rhythm. Much like crafting code, selecting the right combination of melodies requires a keen sense of harmony and an understanding of the audience's needs.
+ <br></br><strong>Philosophy:</strong><br></br>
+My approach to software engineering is grounded in the philosophy that technology should serve as an enabler, simplifying lives and fostering meaningful connections. This ethos extends beyond the screen, guiding my creative pursuits in music and poetry.
+<br></br>As you navigate through this digital space, I invite you to explore the intersections of my professional endeavors and personal passions. Together, let's embark on a journey where technology meets artistry, and where every line of code is infused with both precision and poetry. Welcome to the harmonious convergence of logic and creativity.
+                </p>
+                   
             </div>
             <div className=' my-18 py-10'>
                 <AnimatePresence>               
