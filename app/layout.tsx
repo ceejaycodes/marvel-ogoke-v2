b9c8218c6a32b9d528abc9b64ce41f99640d38d7
Marvel <dreelceejay79@yahoo.com>
2023-12-14 09:04:45 +0100

    nav styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   return(
-    <html lang="en">
+    <html className='bg-backdark' lang="en">
       <body className={`${grotesk.className}`}>
         <Hydrate>
       <Nav/>
