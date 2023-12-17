b4c5276a418d4ecf26071da559a532defbb26861
Marvel <dreelceejay79@yahoo.com>
2023-12-17 02:04:34 +0100

    connect section

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -22,16 +22,16 @@ const Contact = (props: Props) => {
         </motion.div>
         </motion.div>
 
-        <div className='flex my-18 py-10 flex-wrap text-slate-400 px-9 justify-evenly items-center bg-slate-800 bg-opacity-70'>
-            <div className='lg:w-1/2 m-4'>
-                <div className='h-40 w-80'> <Lottie animationData={Connect}/></div>
-            </div>
-            <div className=' my-18'>
-            
-          <a target='_blank' href='https://github.com/ceejaycodes'> <span className='flex gap-4 justify-center items-center '><Image className='h-12 w-12' src={Github} alt={'github'}/><h1 className='text-2xl'>Github</h1></span></a> 
-          <a target='_blank' href='https://linkedin.com/in/marvel-ogoke'>  <span className='flex gap-4 justify-center items-center ml-5 '><Image className='h-10 w-10' src={LinkedIn} alt={'LinkedIn'}/><h1 className='text-2xl'>LinkedIn</h1></span></a> 
-           <a target='_blank' href='mailto:dreelceejay79@yahoo.com'> <span className='flex gap-4 justify-center items-center mr-5 '><Image className='h-12 w-12' src={Email} alt={'mail'}/><h1 className='text-2xl'>Mail</h1></span></a> 
- 
+        <div className='flex flex-wrap text-slate-400 justify-evenly items-end  bg-black'>
+        <div className='h-full w-full opacity-30'> <Lottie animationData={Connect}/></div>
+           
+            <div className='absolute bg-slate-800 bg-opacity-70 p-8 mt-16  lg:m-8 rounded-2xl  md:mt-72  h-auto w-full '>
+            <div className='ml-24'> <h1 className='md:text-2xl'>You can find me here ␥ </h1></div>
+            <div className='flex flex-row gap-10 lg:gap-20 pt-7  p-6'>
+          <a target='_blank' href='https://github.com/ceejaycodes'> <span className='flex lg:gap-4 justify-center items-center '><Image className='lg:h-12 lg:w-12' src={Github} alt={'github'}/><h1 className='lg:text-2xl'>Github</h1></span></a> 
+          <a target='_blank' href='https://linkedin.com/in/marvel-ogoke'>  <span className='flex lg:gap-4 justify-center items-center ml-5 '><Image className='lg:h-10 lg:w-10' src={LinkedIn} alt={'LinkedIn'}/><h1 className='lg:text-2xl'>LinkedIn</h1></span></a> 
+           <a target='_blank' href='mailto:dreelceejay79@yahoo.com'> <span className='flex lg:gap-4 justify-center items-center mr-5 '><Image className='lg:h-12 lg:w-12' src={Email} alt={'mail'}/><h1 className='lg:text-2xl'>Mail</h1></span></a> 
+           </div>
             </div>
         </div>
 </div>
