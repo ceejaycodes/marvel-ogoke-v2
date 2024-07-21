af3029ecb769a50da3fa309bebfa1dccff0b3fde
ceejaycodes <dreelceejay79@yahoo.com>
2024-07-21 04:27:09 +0100

    21/07/2024

--- app/components/utils/Hydrate.tsx
+++ app/components/utils/Hydrate.tsx
@@ -15,7 +15,7 @@ export default function Hydrate({children}: {children: ReactNode}){
     
     return(
         <>
-    {isHydrated ? <div className={`flex flex-col min-h-screen`}><div className="bg-black w-full h-screen fixed bg-center bg-cover  lg:bg-bottom flex justify-center align-bottom"><Lottie className='opacity-30' animationData={backanim} /></div><div className={`flex-grow  w-full h-full  relative`}>{children} </div></div> : <div className='h-96 w-96'><Lottie animationData={Animate}/></div>}
+    {isHydrated ? <div className={`flex flex-col min-h-screen`}><div className="bg-black w-full h-screen fixed bg-center bg-cover  lg:bg-bottom flex justify-center align-bottom"><Lottie className='opacity-30' animationData={backanim} /></div><div className={`flex-grow  w-full h-full  relative`}>{children} </div></div> : <div className='h-96 w-96 flex justify-center align-middle p-40'><Lottie animationData={Animate}/></div>}
     </>
     )
 }
